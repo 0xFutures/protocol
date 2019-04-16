78f05e872138602284f5c5eab520aea663c5cfc9
Chris Hatch <hatch@tuta.io>
2019-04-16 10:42:12 +1000

    fix truffle.js: set kovan network only if config.kovan.json is present

--- truffle.js
+++ truffle.js
@@ -1,13 +1,51 @@
 require('babel-register')
 require('babel-polyfill')
 
+const fs = require('fs')
+
 const HDWalletProvider = require('truffle-hdwallet-provider')
 
 const configDevelop = require('./config.develop.json')
 const configTest = require('./config.test.json')
-const configKovan = require('./config.kovan.json')
 
 const defaultGasLimit = 4000000 // overridden in deployment scripts where needed
+
+const networks = {
+  develop: {
+    host: 'localhost',
+    port: 9545,
+    network_id: '*',
+    from: configDevelop.ownerAccountAddr,
+    gasLimit: 7000000
+  },
+  test: {
+    host: 'localhost',
+    port: 7545,
+    network_id: '*',
+    from: configTest.ownerAccountAddr,
+    gasLimit: 7000000
+  }
+}
+
+// add kovan network if config file has been created
+const configFileKovan = './config.kovan.json'
+if (fs.existsSync(configFileKovan)) {
+  const configKovan = require(configFileKovan)
+  networks.kovan = {
+    provider: function () {
+      return new HDWalletProvider(
+        configKovan.hdWalletMnemonic,
+        configKovan.rpcAddr,
+        0,
+        5
+      )
+    },
+    network_id: 42,
+    from: configKovan.ownerAccountAddr,
+    gas: defaultGasLimit,
+    gasPrice: configKovan.gasPrice
+  }
+}
 
 module.exports = {
   solc: {
@@ -17,34 +55,5 @@ module.exports = {
       runs: 200
     }
   },
-  networks: {
-    develop: {
-      host: 'localhost',
-      port: 9545,
-      network_id: '*',
-      from: configDevelop.ownerAccountAddr,
-      gasLimit: 7000000
-    },
-    test: {
-      host: 'localhost',
-      port: 7545,
-      network_id: '*',
-      from: configTest.ownerAccountAddr,
-      gasLimit: 7000000
-    },
-    kovan: {
-      provider: function () {
-        return new HDWalletProvider(
-          configKovan.hdWalletMnemonic,
-          configKovan.rpcAddr,
-          0,
-          5
-        )
-      },
-      network_id: 42,
-      from: configKovan.ownerAccountAddr,
-      gas: defaultGasLimit,
-      gasPrice: configKovan.gasPrice
-    },
-  }
+  networks
 }
